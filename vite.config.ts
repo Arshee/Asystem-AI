aeb869954bccfaa640c33bde7e15db59a816b0c6
Arshee <areksoroka@wp.pl>
2025-11-07 20:59:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  
+  const env = loadEnv(mode, ".", "VITE_");
+
   return {
     server: {
       port: 3000,
-      host: '0.0.0.0',
+      host: "0.0.0.0",
     },
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-      'process.env.OPENAI_API_KEY': JSON.stringify(env.OPENAI_API_KEY)
+      "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    }
+        "@": path.resolve(__dirname, "."),
+      },
+    },
   };
 });
